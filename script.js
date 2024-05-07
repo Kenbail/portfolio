e08cdd17bfe9cb4df558a36311d4e7b0aea574a6
Kenbail <lucasgoue@gmail.com>
2024-05-07 08:15:22 +0200

    ajout chargement et mode nuit

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 $(document).ready(function () {
-    $(".orange.texte").mouseleave(function () {
+    setTimeout(() => {
+        $(".loading-screen").hide()
+        var laclass = $(".orangebackground").attr("class")
+        $('.' + laclass).css("background-color", "lightblue")
+        $("body").css("background-color", "rgb(210,210,210)")
+    }, 3000);
+
+    /*$(".orange.texte").mouseleave(function () {
         var couleur = $(this).css("color");
         $(this).css("color", couleur)
-    })
+    })*/
+
+
     // i pour interval
     var i = 0
-
-    setInterval(() => {
+    $('.gauche').on("click", function () {
         if (i == 0) {
             var laclass = $(".orangebackground").attr("class")
-            $('.' + laclass).css("background-color", "blue")
-            i = i + 1
+            $('.' + laclass).css("background-color", "lightblue")
+            $("body").css("background-color", "rgb(210,210,210)")
+            setTimeout(() => {
+                i = i + 1
+            }, 100);
         } if (i == 1) {
-            var laclass = $(".orangebackground").attr("class")
-            $('.' + laclass).css("background-color", "red")
-            i = i + 1
+            setTimeout(() => {
+                var laclass = $(".orangebackground").attr("class")
+                $('.' + laclass).css("background-color", "orange")
+                $("body").css("background-color", "black")
+                i = 0
+            }, 100);
         }
-        if (i == 2) {
-            var laclass = $(".orangebackground").attr("class")
-            $('.' + laclass).css("background-color", "red")
-            i = i - 2
-        }
-    }, 4000);
+    })
+
 })
 
 
